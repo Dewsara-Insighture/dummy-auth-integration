dd813719d181e8da0546219f167a75c55e86b793
Dewsara-Insighture <dewsara.wijerathne@insighture.com>
2023-08-21 18:06:29 +0530

    new route pushed

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes ,Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import SignIn from '../pages/auth/SignIn';
 import SignUp from '../pages/auth/SignUp';
 import ErrorPage404 from '../pages/404/404';
-import AuthWrapper from './AuthWrapper';
+// import AuthWrapper from './AuthWrapper';
 import Dashboard from '../pages/dashboard/Dashboard';
 import ConfirmInvitation from '../pages/confirm-invitation/ConfirmInvitation';
-import CreateOrganization from '../pages/create-organization/CreateOrganization';
+// import CreateOrganization from '../pages/create-organization/CreateOrganization';
 
 export default function AppRoutes() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<SignIn />} />
+        <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route
           key="confirm-invitation"
@@ -19,11 +19,11 @@ export default function AppRoutes() {
           element={<ConfirmInvitation />}
         />
         <Route
-          key="forget-password"
-          path="/forgetpassword"
+          key="dashboard"
+          path="/dashboard"
           element={<Dashboard />}
         />
-        <Route
+        {/* <Route
           path="/dashboard/*"
           element={
             <AuthWrapper>
@@ -40,7 +40,7 @@ export default function AppRoutes() {
             </AuthWrapper>
           }
         />
-         <Route path="/" element={<Navigate to='dashboard' />} />
+         <Route path="/" element={<Navigate to='dashboard' />} /> */}
         <Route path="/*" element={<ErrorPage404 />} />
       </Routes>
       {/* <PublicRoutes /> */}
